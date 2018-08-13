9f4db414dd0e64a52740d2e3dd98428ec70f3bc5
hiteshingole <hiteshingole2000@gmail.com>
2018-08-13 17:39:49 +0000

    added user and group resource

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -23,6 +23,21 @@ variables partials: {
     
 end 
 
+
+user 'mapr' do
+	comment 'mapr user'
+	uid '123456789'
+	home '/home/mapr'
+	shell '/bin/bash'
+end
+
+group 'mapr' do
+
+	members 'mapr'
+	gid '123456789'
+	append true
+end
+
 service 'ntpd' do 
 	action [:enable, :start]
 end
